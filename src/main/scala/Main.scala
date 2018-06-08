6315fb162d64560df850e87987d85d04cb3ea9e5
Arturas Smorgun <arturas@spikerlabs.com>
2018-06-08 01:33:50 +0100

    Tidy up main

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -19,22 +19,16 @@ object Main extends App {
       Word("coyote"),
     )
   )
+  val shouldDebug = System.getenv("DEBUG") == "true"
   play(Game(dictionary).start)
 
   @tailrec
   def play(game: Game): Unit = game match {
+    case _: Game.Pending => throw new IllegalStateException("does not supposed to end up in pending state")
     case _: Game.InProgress =>
-      if (System.getenv("DEBUG") == "true") {
-        println(game.dictionary.words)
-        println(
-          game.dictionary.words.map {
-            case Word(value, guesses) => (value, guesses)
-          }
-        )
-      }
+      if (shouldDebug) debug(game.dictionary.words)
       println(Hangman.display(game.numberOfMissesLeft, game.dictionary.toString))
-      val letter = scala.io.StdIn.readChar()
-      play(game.guess(letter))
+      play(game.guess(scala.io.StdIn.readChar()))
     case _: Game.Lost =>
       game.dictionary.words.head match {
         case Word(value, _) => println(Hangman.displayLoss(value))
@@ -42,5 +36,14 @@ object Main extends App {
     case _: Game.Won =>
       println(Hangman.display(game.numberOfMissesLeft, game.dictionary.toString))
       println(Hangman.displayWin)
+  }
+
+  private def debug(words: List[Word]) = {
+    println(words)
+    println(
+      words.map {
+        case Word(value, guesses) => (value, guesses)
+      }
+    )
   }
 }
